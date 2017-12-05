60bba0a6365cdd4d03ffbb3f079a5c4504057e2b
Matt Larson <matt.a.larson@gmail.com>
2017-12-05 14:08:19 -0600

    type hinting

--- messages101/index.php
+++ messages101/index.php
@@ -12,13 +12,22 @@ class Person
 
 class Business
 {
+    protected $staff;
+
+    public function __construct(Staff $staff)
+    {
+        $this->staff = $staff;
+    }
+
+    //Type hinting the object name to specify expected val should be of type Person
     public function hire(Person $person)
     {
-
+        //add $person to the staff collection
+        $this->staff->add($person);
     }
 }
 
 class Staff
 {
- 
+    
 }
