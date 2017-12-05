20c9d5924964cfe4c4eacc5f27c9ae1a8ac78b6d
Matt Larson <matt.a.larson@gmail.com>
2017-12-05 12:14:02 -0600

    episode 1

---
+++ README.md
@@ -0,0 +1,3 @@
+# OOP
+
+$this is a way of referring to "this object" inside a class or more specifically, this instance of our class
--- Task.php
+++ Task.php
@@ -1,9 +1,23 @@
 <?php
 
 class Task {
+
+    public $title;
     public $description;
+    public $completed = false;
+
+    public function __construct($title, $description)
+    {
+        $this->title = $title;
+        $this->description = $description;
+    }
+
+    public function complete($completed)
+    {   
+        $this->completed = $completed;    
+    }
 }
 
-$task = new Task();
+$task = new Task('Learn OOP', 'Watching the laracasts videos to learn OOP in PHP.');
 
-var_dump($task->description);
+var_dump($task);
