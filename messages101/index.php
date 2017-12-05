a6c7bbe7f38cf98342cf916207afcaf45d5a4d5e
Matt Larson <matt.a.larson@gmail.com>
2017-12-05 15:31:38 -0600

    message sending between classes

--- messages101/index.php
+++ messages101/index.php
@@ -4,7 +4,7 @@ class Person
 {
     protected $name;
 
-    public function __construct($namge)
+    public function __construct($name)
     {
         $this->name = $name;    
     }
@@ -25,9 +25,40 @@ class Business
         //add $person to the staff collection
         $this->staff->add($person);
     }
+
+    public function getStaffMembers()
+    {
+        return $this->staff->getMembers();
+    }
 }
 
 class Staff
 {
-    
+    protected $members = [];
+
+    public function __construct($members = []) 
+    {
+        $this->members = $members;
+    }
+
+    public function add(Person $person)
+    {
+        $this->members[] = $person;
+    }
+
+    public function getMembers()
+    {
+        return $this->members;
+    }
 }
+
+//Business depends on the staff in order to run
+
+$matt = new Person('Matt Larson');
+$staff = new Staff([$matt]);
+$usi = new Business($staff);
+
+$usi->hire(new Person('Jane Doe'));
+$usi->hire(new Person('Sarah White'));
+
+var_dump($usi->getStaffMembers());
