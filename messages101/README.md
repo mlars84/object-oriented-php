16f1d0e21c3c1c782f8883eecd83024ec1cfd947
Matt Larson <matt.a.larson@gmail.com>
2017-12-05 13:57:00 -0600

    working on messages vid

---
+++ messages101/README.md
@@ -0,0 +1,9 @@
+# Messages
+
+Core component of Object Oriented Design
+
+PHP Type Hinting
+    - Add object name before variable when passing to child methods:
+    - ```class Child{
+        public function hire(Parent $parent)
+    }```
---
+++ messages101/index.php
@@ -0,0 +1,24 @@
+<?php 
+
+class Person
+{
+    protected $name;
+
+    public function __construct($namge)
+    {
+        $this->name = $name;    
+    }
+}
+
+class Business
+{
+    public function hire(Person $person)
+    {
+
+    }
+}
+
+class Staff
+{
+ 
+}
