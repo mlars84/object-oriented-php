48e3a92ccb3c3fd73a16d27cb07fd7d911fadef2
Matt Larson <matt.a.larson@gmail.com>
2017-12-06 07:57:18 -0600

    refactored index

---
+++ messages101/ex.php
@@ -0,0 +1,10 @@
+<?php 
+//Business depends on the staff in order to run
+$matt = new USI\Person('Matt Larson');
+$staff = new USI\Staff([$matt]);
+$usi = new USI\Business($staff);
+
+$usi->hire(new USI\Person('Jane Doe'));
+$usi->hire(new USI\Person('Sarah White'));
+
+var_dump($usi->getStaffMembers());
--- messages101/index.php
+++ messages101/index.php
@@ -1,12 +1,4 @@
-<?php 
+<?php
+
 require 'vendor/autoload.php';
-
-//Business depends on the staff in order to run
-$matt = new USI\Person('Matt Larson');
-$staff = new USI\Staff([$matt]);
-$usi = new USI\Business($staff);
-
-$usi->hire(new USI\Person('Jane Doe'));
-$usi->hire(new USI\Person('Sarah White'));
-
-var_dump($usi->getStaffMembers());
+require 'ex.php';
